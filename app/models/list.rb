03d39745e990f46594ea0c156f397fa9f70f753e
Abe Anderson <aberanderson@gmail.com>
2016-02-27 15:12:52 -0500

    actually I think I got it (CHECKPOINT 6 DONE)

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,7 @@ class List < ActiveRecord::Base
   has_many :items
   belongs_to :user
 
-  ACCEPTED_TERMS = /(private)|(viewable)|(open)|(public)/ #I added public here, because I didn't understand the assignment properly at first. I don't feel like raking the whole db and changing all that again. I always can though. 
+  ACCEPTED_TERMS = /(private)|(viewable)|(open)|(public)/ #I added public here, because I didn't understand the assignment properly at first. I don't feel like raking the whole db and changing all that again. I always can though.
 
   validates :title, length: {minimum: 1, maximum: 40 }, presence: true
   validates :permissions,
