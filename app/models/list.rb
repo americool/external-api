8257afdbff73c9bd0d05959ef86ba9012a247f74
Abe Anderson <aberanderson@gmail.com>
2016-02-27 15:10:20 -0500

    finished one problem just gotta figure out item routes stuff

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,11 +2,11 @@ class List < ActiveRecord::Base
   has_many :items
   belongs_to :user
 
-  PUBLIC_VALIDATOR = /(public)|(Public)|(PUBLIC)/
+  ACCEPTED_TERMS = /(private)|(viewable)|(open)|(public)/ #I added public here, because I didn't understand the assignment properly at first. I don't feel like raking the whole db and changing all that again. I always can though. 
 
   validates :title, length: {minimum: 1, maximum: 40 }, presence: true
   validates :permissions,
-            format: {with: PUBLIC_VALIDATOR},
+            format: {with: ACCEPTED_TERMS},
             on: :update
 
 end
